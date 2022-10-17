4782a6cdf08aa69e533f76ffa89e0a5bf2e388e4
deceser <aufhaummtobsc@gmail.com>
2022-10-17 13:37:46 +0300

    deploy

--- server.js
+++ server.js
@@ -1,11 +1,24 @@
+// const express = require("express");
+// const app = express();
+// const path = require("path");
+// const port = process.env.PORT || 5000;
+// app.use(express.static("public"));
+// app.get("*", (req, res) => {
+//   res.sendFile(path.resolve(__dirname, "public", "index.html"));
+// });
+// app.listen(port, () =>
+//   console.log(`Server is running on: http://localhost:${port}`)
+// );
+
+const path = require("path");
 const express = require("express");
 const app = express();
-const path = require("path");
-const port = process.env.PORT || 5000;
-app.use(express.static("public"));
+const publicPath = path.join(__dirname, "..", "public");
+const port = process.env.PORT || 3000;
+app.use(express.static(publicPath));
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "public", "index.html"));
+  res.sendFile(path.join(publicPath, "index.html"));
 });
-app.listen(port, () =>
-  console.log(`Server is running on: http://localhost:${port}`)
-);
+app.listen(port, () => {
+  console.log("Server is up!");
+});
