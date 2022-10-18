c9d047be3c4ebe6a47976a5811e69a1991f92fd0
deceser <aufhaummtobsc@gmail.com>
2022-10-18 15:44:59 +0300

    deploy

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 const express = require("express");
-const app = express();
+const favicon = require("express-favicon");
 const path = require("path");
-const port = process.env.PORT || 5000;
-app.use(express.static("public"));
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "public", "index.html"));
+const port = process.env.PORT || 3000;
+
+// здесь у нас происходит импорт пакетов и определяется порт нашего сервера
+const app = express();
+app.use(favicon(__dirname + "/build/favicon.png"));
+
+//здесь наше приложение отдаёт статику
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, "build")));
+
+//простой тест сервера
+app.get("/ping", function (req, res) {
+  return res.send("pong");
 });
-app.listen(port, () =>
-  console.log(`Server is running on: http://localhost:${port}`)
-);
+
+//обслуживание html
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+app.listen(port);
