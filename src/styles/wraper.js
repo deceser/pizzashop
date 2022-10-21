4a46c55035aa5098565f6dc2faa7173161b8846f
deceser <aufhaummtobsc@gmail.com>
2022-10-21 12:19:38 +0300

    wraper

--- src/styles/wraper.js
+++ src/styles/wraper.js
@@ -4,6 +4,7 @@ import { Container } from "@mui/system";
 export const Container__Wraper = styled(Container)(() => ({
   padding: "0px",
   backgroundColor: "#fff",
+  borderRadius: "10px",
   ["@media screen and (min-width: 1000px) and (max-width: 1115px)"]: {
     width: "800px",
   },
