e58495d97c8fad91641c6d916e3a107ad97c7fb8
deceser <aufhaummtobsc@gmail.com>
2022-10-21 12:38:04 +0300

    autoplayslide

--- src/components/slider/index.jsx
+++ src/components/slider/index.jsx
@@ -28,9 +28,12 @@ const styles = {
 };
 
 const settings = {
-  speed: 500,
+  speed: 2000,
   slidesToShow: 1,
   slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 5000,
+  pauseOnHover: true,
   nextArrow: <SampleNextArrow />,
   prevArrow: <SamplePrevArrow />,
 };
