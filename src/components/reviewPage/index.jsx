3718f2185809a8af8927e7519822e1cb31ee2ca6
deceser <aufhaummtobsc@gmail.com>
2022-11-03 16:50:47 +0200

    text field

--- src/components/reviewPage/index.jsx
+++ src/components/reviewPage/index.jsx
@@ -1,5 +1,10 @@
 import React from "react";
 
+import Box from "@mui/system/Box";
+import Paper from "@mui/material/Paper";
+
+import { Button__BuyNow } from "../../styles/button";
+
 import { Navigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { isAuthSelect } from "../../redux/slices/authSlice";
@@ -9,11 +14,39 @@ import "easymde/dist/easymde.min.css";
 
 const Review = () => {
   const isAuth = useSelector(isAuthSelect);
+  const [value, setValue] = React.useState("");
+
+  const handleChangeFile = () => {};
+
+  const onCliclRemoveImage = () => {};
+
+  const onChange = React.useCallback((value) => {
+    setValue(value);
+  });
+
+  const option = React.useMemo(() => ({
+    spellChecker: false,
+    autofocus: true,
+    placeholder: "",
+    status: false,
+    autosave: {
+      enabled: true,
+      delay: 1000,
+    },
+  }));
+
   if (!isAuth) {
     return <Navigate to="/" />;
   }
 
-  return <SimpleMDE />;
+  return (
+    <Paper elevation={0} style={{ padding: 30 }}>
+      <SimpleMDE value={value} onChange={onChange} options={option} />
+      <Box sx={{ mt: 2 }}>
+        <Button__BuyNow>Send</Button__BuyNow>
+      </Box>
+    </Paper>
+  );
 };
 
 export default Review;
